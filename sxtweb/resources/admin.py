cd3c7aeeb49cc6dc3cd687b6a76ad1b93f08d3c0
Lixin Zhan <lixinzhan@gmail.com>
2018-11-30 14:31:39 -0500

    Add all files in resources app

---
+++ sxtweb/resources/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(MACHINE)
+admin.site.register(FILTER)
+admin.site.register(CONE)
+admin.site.register(CHAMBER)
+admin.site.register(ELECTROMETER)
+admin.site.register(DOCTOR)
+
